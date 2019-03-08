48ed3d0694f7e845bbdfee47b232e3187f5a5f34
dwleonhardt <dwleonhardt@gmail.com>
2019-03-08 13:34:43 -0700

    rename react project and fix react typescript compilation errors

--- my-app/src/store/store.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var redux_1 = require("redux");
-var redux_logger_1 = require("redux-logger");
-var root_reducer_1 = require("../reducers/root-reducer");
-var store = redux_1.createStore(root_reducer_1.default, redux_1.applyMiddleware(redux_logger_1.default));
-exports.default = store;
--- plotly-test/dist/store/store.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const redux_1 = require("redux");
-const redux_logger_1 = require("redux-logger");
-const root_reducer_1 = require("../reducers/root-reducer");
-const store = redux_1.createStore(root_reducer_1.default, redux_1.applyMiddleware(redux_logger_1.default));
-exports.default = store;
-//# sourceMappingURL=store.js.map
